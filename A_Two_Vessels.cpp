cae276e325a0a5e33c3242e4af311fdf777bd930
RakibulHassan29052000 <rakobulsagor@gamil.com>
2023-09-09 10:21:01 +0600

    new_codes

---
+++ A_Two_Vessels.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define R_H Rakibul Hassan
+#define ll long long
+#define nl endl
+#define pii pair<int, int>
+#define all_occarance (ctrl + shift + a)
+#define f1(n) for (int i = 0; i < n; i++)
+#define f2(a, n) for (int i = a; i < n; i++)
+using namespace std;
+void solve()
+{
+
+    int a, b, c, res, ans;
+    cin >> a >> b >> c;
+    res = (abs(a - b) + 1) / 2;
+    ans = (abs(res + c - 1)) / c;
+    cout << ans << endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
