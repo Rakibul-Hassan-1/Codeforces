31df29453d717ce7729ebd7b05bc0c271a72baa6
Rakibul-Hassan-1 <rakibulhassan29052000@gmail.com>
2024-03-01 17:18:37 +0600

    new contest are added also added NCPC 2024

---
+++ Codeforces Round 930 (Div. 2)/B_Binary_Path.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define endl '\n'
+#define fasterIO()                 \
+    ios_base ::sync_with_stdio(0); \
+    cin.tie(0);                    \
+    cout.tie(0)
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+vector<int> getDivisors(int n)
+{
+    vector<int> d;
+    for (int i = 1; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            d.push_back(i);
+            if (i != (n / i))
+            {
+                d.push_back(n / i);
+            }
+        }
+    }
+    return d;
+}
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    string s, s2;
+    cin >> s >> s2;
+    map<string, int> mp;
+    for (int i = 0; i < n; i++)
+    {
+        string ans = s.substr(0, i + 1) + s2.substr(i);
+        mp[ans]++;
+    }
+    auto minimuString = mp.begin();
+    cout << minimuString->first << endl;
+    cout << minimuString->second << endl;
+}
+int main()
+{
+    fasterIO();
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+        solve();
+
+    return 0;
+}
